5c3f3ff3c9be6a9db4837dee3888b4c01edbf045
harryk2142 <>
2025-03-22 17:50:01 +0100

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 import dotenv from "dotenv";
+import preact from '@astrojs/preact';
 
 import sitemap from "@astrojs/sitemap";
 
@@ -9,42 +10,45 @@ dotenv.config(); // load env vars from .env
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://harryk2142.github.io",
-  site: "https://the-ai-files.de",
-  base: "/",
-  root: ".",
-  markdown: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
-  },
-  integrations: [
-    mdx({
-      remarkPlugins: [],
-      rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
-    }),
-    sitemap({
-      filter: (page) => !page.includes("tags") && !page.includes("api"),
-    }),
-  ],
-  server: {
-    port: 3000,
-    // open: true,
-  },
+    // site: "https://harryk2142.github.io",
+    site: "https://the-ai-files.de",
+    base: "/",
+    root: ".",
+    markdown: {
+        remarkPlugins: [],
+        rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+    },
+    integrations: [
+        mdx({
+            remarkPlugins: [],
+            rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+        }),
+        sitemap({
+            filter: (page) => !page.includes("tags") && !page.includes("api"),
+        }),
+        preact({
+            compat: true
+        }),
+    ],
+    server: {
+        port: 3000,
+        // open: true,
+    },
 
-  vite: {
-    define: {
-      FB_API_KEY: JSON.stringify(process.env.FIREBASE_COMMENTS_API_KEY),
-      FB_PROJECT_ID: JSON.stringify(process.env.FIREBASE_COMMENTS_PROJECT_ID),
-      DEBUG: process.env.DEBUG,
-    },
-    build: {
-      modulePreload: false,
-      rollupOptions: {
-        output: {
-          assetFileNames: "assets/[name][extname]",
-          inlineDynamicImports: false,
+    vite: {
+        define: {
+            FB_API_KEY: JSON.stringify(process.env.FIREBASE_COMMENTS_API_KEY),
+            FB_PROJECT_ID: JSON.stringify(process.env.FIREBASE_COMMENTS_PROJECT_ID),
+            DEBUG: process.env.DEBUG,
         },
-      },
+        build: {
+            modulePreload: false,
+            rollupOptions: {
+                output: {
+                    assetFileNames: "assets/[name][extname]",
+                    inlineDynamicImports: false,
+                },
+            },
+        },
     },
-  },
 });
